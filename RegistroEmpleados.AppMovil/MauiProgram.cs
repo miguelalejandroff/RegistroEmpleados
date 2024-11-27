5e877cabf730bb4751f9e7269d595c278f3e04ad
Miguel Fuentealba <miguelalejandroff@gmail.com>
2024-11-27 19:18:52 -0300

    Agregar archivos de proyecto.

---
+++ RegistroEmpleados.AppMovil/MauiProgram.cs
@@ -0,0 +1,42 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using Microsoft.Extensions.Logging;
+using RegistroEmpleados.Modelos.Modelos;
+
+namespace RegistroEmpleados.AppMovil
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+            Registrar();
+            return builder.Build();
+
+        }
+        public static void Registrar()
+        {
+            FirebaseClient client = new FirebaseClient("https://registroempleados-b0faf-default-rtdb.firebaseio.com/");
+            var cargos = client.Child("Cargos").OnceAsync<Cargo>();
+
+            if(cargos.Result.Count == 0)
+            {
+                client.Child("Cargos").PostAsync(new Cargo { Nombre = "Administrador" });
+                client.Child("Cargos").PostAsync(new Cargo { Nombre = "Supervisor" });
+                client.Child("Cargos").PostAsync(new Cargo { Nombre = "Usuario" });
+            }
+        }
+
+    }
+}
---
+++ RegistroEmpleados.AppMovil/Vistas/ListarEmpleados.xaml.cs
@@ -0,0 +1,19 @@
+namespace RegistroEmpleados.AppMovil.Vistas;
+
+public partial class ListarEmpleados : ContentPage
+{
+	public ListarEmpleados()
+	{
+		InitializeComponent();
+	}
+
+    private void filtroSearchBar_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void NuevoEmpleadoBoton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
